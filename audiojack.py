fd3e53d0e56c635172a67358355b1676589e6dac
Gautam Mekkat <Blue9@users.noreply.github.com>
2015-10-11 17:29:57 -0400

    Added documentation

--- audiojack.py
+++ audiojack.py
@@ -16,24 +16,26 @@ opts = {
         'preferredquality': '256'
     }],
 }
-cover_art_dict = {} 
-url = '' 
-artist_title = [] 
-results = [] 
-ydl = youtube_dl.YoutubeDL(opts) 
-id = '' 
-title = '' 
+cover_art_dict = {} # dictionary of all found cover art images as binary data
+url = '' # requested url
+artist_title = [] # [artist name, title name] (parsed from video title)
+results = [] # list of all found songs
+ydl = youtube_dl.YoutubeDL(opts) # object that downloads YouTube/SoundCloud/etc. video
+id = '' # video id, used for naming file
+title = '' # title of song
 
 def set_useragent(name, version):
+    '''Must do this before anything else!!!'''
     musicbrainzngs.set_useragent(name, version)
 
 def get_results(u):
+    '''Start here (after setting user agent).'''
     global url
     global results
     global id
     url = u
     url_info = ydl.extract_info(url, download=False)
-    if 'entries' in url_info: 
+    if 'entries' in url_info: # If the URL is a playlist, only retrieve the first video (May change later).
         url_info = url_info['entries'][0]
     id = url_info['id']
     artist_title = get_artist_title(url_info)
@@ -44,23 +46,23 @@ def download(url, id):
     global opts
     global title
     if title != '':
-        file = '%s.temp' % title 
+        file = '%s.temp' % title # Does not matter what the extension is. FFMpeg will convert it to MP3 anyway.
         opts['outtmpl'] = file
     ydl = youtube_dl.YoutubeDL(opts)
     ydl.download([url])
     return file
 
 def get_artist_title(url_info):
-    if url_info['uploader'][0] == '#':
+    if url_info['uploader'][0] == '#': # This means it is an official music upload, and the uploader's name will be equivalent to the artist's name.
         artist_title = [url_info['uploader'], url_info['title']]
     else:
-        title = re.sub(r'\(| \([^)]*\)|\) ', '', url_info['title']) 
-        title = re.sub(r'\[| \[[^\]]*\]|\] ', '', title) 
-        banned_words = ['lyrics', 'hd', 'hq', '320kbps', 'free download', 'download', '1080p', '720p'] 
+        title = re.sub(r'\(| \([^)]*\)|\) ', '', url_info['title']) # Remove everything in between parentheses because they could interfere with the search (i.e. remove "official music video" from the video title)
+        title = re.sub(r'\[| \[[^\]]*\]|\] ', '', title) # Same as above but with brackets
+        banned_words = ['lyrics', 'hd', 'hq', '320kbps', 'free download', 'download', '1080p', '720p'] # Remove all words that could interfere with the search
         for word in banned_words:
             while word in title.lower():
                 title = title.replace(word, '')
-        artist_title = re.split(' - | : |- |: ', title)[:2] 
+        artist_title = re.split(' - | : |- |: ', title)[:2] # Most songs are uploaded as ARTIST - TITLE or something similar.
         if len(artist_title) == 1:
             return ['', artist_title[0]]
         artist_title[0] = artist_title[0].split(' & ')[0]
@@ -88,6 +90,7 @@ def get_metadata(artist_title):
     return results
 
 def valid(recording, release, entry):
+    '''Checks to make sure the result is not an instrumental, remix, compilation, etc. Also requires cover art for the song to be deemed valid.'''
     banned = ['instrumental', 'best of', 'diss', 'remix', '2015', '2016', '2k15', '2k16', 'what i call']
     for word in banned:
         if word in entry[1].lower() or word in entry[2].lower():
@@ -101,6 +104,7 @@ def valid(recording, release, entry):
     return True
 
 def get_cover_art_as_data(id):
+    '''Gets cover art as binary data if not already stored.'''
     global cover_art_dict
     if id in cover_art_dict:
         return cover_art_dict[id]
@@ -111,6 +115,7 @@ def get_cover_art_as_data(id):
     return cover_art_dict[id]
 
 def select(index):
+    '''Select the metadata to be added to the MP3.'''
     global title
     selection = results[index]
     title = selection[1]
@@ -126,6 +131,7 @@ def select(index):
     return file
 
 def custom(artist, custom_title, album):
+    '''Custom metadata to be added to the MP3.'''
     global title
     title = custom_title;
     file = '%s.mp3' % title
